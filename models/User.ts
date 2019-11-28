9b379c33e0a1f1a848c539a3c25587c9f506d9ea
kimsungill <48203864+kimsungill@users.noreply.github.com>
2019-11-28 15:08:43 +0900

    수정
    수정

---
+++ models/User.ts
@@ -0,0 +1,24 @@
+// 사용자 등록시 서버에 전송하는 정보의 데이터 모델.
+export class User {
+  userid: string;
+  username: string;
+  email: string;
+  phone: number;
+  password: string;
+}
+
+// 로그인시 서버에 보내는 정보의 데이터 모델
+export class Login {
+  userid: string;
+  password: string;
+}
+
+// 로그인된 사용자의 데이터 모델.
+// 패스워드 정보는 보안을 위해 삭제하고 사용함.
+export class UserNoPW {
+  userid: string;
+  username: string;
+  email: string;
+  phone: number;
+  password: string;
+}
